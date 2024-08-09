487cf929fce0425be81e0a45b6bd2cffc29edf56
humandavey <davey.adams.three@gmail.com>
2024-08-08 22:17:45 -0500

    change: stop hunger while waiting

--- src/main/java/me/humandavey/minigamelib/game/Game.java
+++ src/main/java/me/humandavey/minigamelib/game/Game.java
@@ -14,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
@@ -101,6 +102,15 @@ public abstract class Game implements Listener {
                     event.setCancelled(true);
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onHungerChange(FoodLevelChangeEvent event) {
+        if (getState() == GameState.LIVE) return;
+
+        if (getPlayers().contains((Player) event.getEntity())) {
+            event.setCancelled(true);
         }
     }
 
